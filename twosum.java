29399478bb5498117988e58fc0a3fdf7d46cbbda
kunjicoder <140734704+kunjicoder@users.noreply.github.com>
2023-07-27 20:23:59 +0400

    Update twosum.java

--- twosum.java
+++ twosum.java
@@ -1,11 +1,11 @@
-//LeetCode problem find the element locations which provides target sum
-/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+#LeetCode problem find the element locations which provides target sum
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-You can return the answer in any order.
+#You can return the answer in any order.
 
-Example 1:
+/*Example 1:
 
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
